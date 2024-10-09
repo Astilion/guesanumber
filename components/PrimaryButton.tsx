2eab7bf23d383fca0426f30eb4671d3f71a9cbcb
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-10-09 20:20:25 +0200

    styled buttons

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,15 +1,50 @@
 import { ReactNode } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 type PrimaryButtonProps = {
   children: ReactNode;
 };
 function PrimaryButton({ children }: PrimaryButtonProps) {
+  const pressButtonHandler = () => {
+    console.log('Pressed');
+  };
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressButtonHandler}
+        android_ripple={{ color: '#333232' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#640233',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  // IOS onpress styling
+  pressed: {
+    opacity: 0.75,
+  },
+});
