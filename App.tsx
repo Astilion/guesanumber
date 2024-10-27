1d51ef391ed2b5078c964b8370082ce3e2011b8d
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-10-27 15:39:52 +0100

    added SafeAreaView for the first time

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState<number | null>(null)
+  const [userNumber, setUserNumber] = useState<number | null>(null);
 
-  const pickedNumberHandler = (pickedNumber: number) => { 
-    setUserNumber(pickedNumber)
-   }
-   let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
+  const pickedNumberHandler = (pickedNumber: number) => {
+    setUserNumber(pickedNumber);
+  };
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
-   if (userNumber) {
-    screen = <GameScreen/>
-   }
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
       <ImageBackground
@@ -24,7 +24,9 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
+        <SafeAreaView style={styles.rootScreen}>
         {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
@@ -36,5 +38,5 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     opacity: 0.15,
-  }
+  },
 });
