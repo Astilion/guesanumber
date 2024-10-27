be4612ceee8a4f97aee0b406e034573c7b0bf2db
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-10-27 13:01:39 +0100

    added Game screen transition

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -2,7 +2,11 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+type StartGameScreenProps = {
+  onPickNumber: (number: number) => void;
+};
+
+function StartGameScreen({ onPickNumber }: StartGameScreenProps) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   const numberInputHandler = (enteredText: string) => {
@@ -21,11 +25,11 @@ function StartGameScreen() {
       Alert.alert(
         'Invalid number!',
         'Number has to be a number between 1 and 99.',
-        [{ text: ' Okay', style:'destructive', onPress: resetInputHandler }],
+        [{ text: ' Okay', style: 'destructive', onPress: resetInputHandler }],
       );
       return;
     }
-    console.log('Valid number')
+    onPickNumber(chosenNumber);
   };
   return (
     <View style={styles.inputContainer}>
