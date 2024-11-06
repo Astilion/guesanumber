4ff5be6271734cb5d3219c88710227e466b10ea6
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-11-06 20:55:29 +0100

    introduced Dimensions api

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
 import Colors from '../../constants/Colors';
 
 type CardProps = {
@@ -11,10 +11,12 @@ function Card({ children }: CardProps) {
 
 export default Card;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   inputContainer: {
     alignItems: 'center',
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
